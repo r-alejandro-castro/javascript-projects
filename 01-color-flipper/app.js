7d252dce8cdc4a2b7fb491f2e7c3c62c60c9f057
Rodolfo Castro <soyracm@gmail.com>
2024-09-10 11:29:14 -0400

    01 color flippers finish

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -8,13 +8,11 @@ btn.addEventListener( 'click', () => {
     // console.log(document.body)
 
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+    // console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 
 });
 
-const getRandomNumber = () => {
-    return Math.random();
-}
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
